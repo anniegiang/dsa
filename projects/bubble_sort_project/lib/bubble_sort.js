769ac20d81edff4593a71c8a89edff69ef30006d
Annie Giang <annieg4123@gmail.com>
2019-11-23 15:27:45 -0800

    Solve Bubble Sort

--- projects/bubble_sort_project/lib/bubble_sort.js
+++ projects/bubble_sort_project/lib/bubble_sort.js
@@ -1,13 +1,24 @@
 function swap(array, idx1, idx2) {
-
+	[array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+	return array;
 }
 
 function bubbleSort(array) {
+	let swapped = true;
 
+	while (swapped) {
+		swapped = false;
+		for (let i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				swapped = true;
+				array = swap(array, i, i + 1);
+			}
+		}
+	}
+	return array;
 }
 
-
 module.exports = {
-    bubbleSort,
-    swap
+	bubbleSort,
+	swap
 };
