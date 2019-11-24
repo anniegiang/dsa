9d1b457acc17c29a619ef252d5deecc91a796c93
Annie Giang <annieg4123@gmail.com>
2019-11-24 15:11:47 -0800

    Solve Insertion Sort

--- projects/insertion_sort_project/lib/insertion_sort.js
+++ projects/insertion_sort_project/lib/insertion_sort.js
@@ -1,10 +1,10 @@
 function insertionSort(arr) {
 	for (let i = 1; i < arr.length; i++) {
-		let currElement = arr[i];
-		for (var j = i - 1; j >= 0 && currElement < arr[j]; j--) {
+		let curEl = arr[i];
+		for (var j = i - 1; j >= 0 && arr[j] > curEl; j--) {
 			arr[j + 1] = arr[j];
 		}
-		arr[j + 1] = currElement;
+		arr[j + 1] = curEl;
 	}
 	return arr;
 }
