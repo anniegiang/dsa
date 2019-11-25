01509a356d300d1cd2be372c70ac3728fe84e5a5
Annie Giang <annieg4123@gmail.com>
2019-11-24 16:58:38 -0800

    Solve Counting Sort

--- projects/counting_sort_project/lib/counting_sort.js
+++ projects/counting_sort_project/lib/counting_sort.js
@@ -1,8 +1,21 @@
 function countingSort(arr, max) {
+	const result = [];
+	const countArr = new Array(max + 1).fill(0);
 
+	for (let i = 0; i < arr.length; i++) {
+		countArr[arr[i]]++;
+	}
+
+	for (let i = 0; i < countArr.length; i++) {
+		while (countArr[i] > 0) {
+			result.push(i);
+			countArr[i]--;
+		}
+	}
+
+	return result;
 }
 
-
 module.exports = {
-    countingSort
+	countingSort
 };
