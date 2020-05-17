e0268f7cc7ac0cd3ad249c804b8d1429049496c1
Annie <annieg4123@gmail.com>
2020-05-17 01:55:55 -0700

    Use while loop

--- projects/sort/insertion_sort_project/lib/insertion_sort.js
+++ projects/sort/insertion_sort_project/lib/insertion_sort.js
@@ -1,8 +1,10 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currElement = arr[i];
-    for (var j = i - 1; j >= 0 && currElement < arr[j]; j--) {
+    let j = i - 1;
+    while (j >= 0 && currElement < arr[j]) {
       arr[j + 1] = arr[j];
+      j--;
     }
     arr[j + 1] = currElement;
   }
