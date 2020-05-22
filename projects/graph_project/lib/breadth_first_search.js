a1b251a735d044b9a38ca85295e189910c2a8407
Annie <annieg4123@gmail.com>
2020-05-21 21:06:41 -0700

    Finish BST and Graph project

---
+++ projects/graph_project/lib/breadth_first_search.js
@@ -0,0 +1,17 @@
+function breadthFirstSearch(startingNode, targetVal) {
+  let visited = new Set();
+  const queue = [startingNode];
+
+  while (queue.length) {
+    const node = queue.shift();
+    if (visited.has(node)) continue;
+    visited.add(node);
+    if (node.val === targetVal) return node;
+    queue.push(...node.neighbors);
+  }
+  return null;
+}
+
+module.exports = {
+  breadthFirstSearch,
+};
