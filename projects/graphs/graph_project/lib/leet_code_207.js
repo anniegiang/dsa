82247ffe08cbd210a7609d6291ed3a67881f5bfc
Annie <annieg4123@gmail.com>
2020-05-22 15:12:54 -0700

    Finish leet code 207

--- projects/graphs/graph_project/lib/leet_code_207.js
+++ projects/graphs/graph_project/lib/leet_code_207.js
@@ -2,5 +2,34 @@
 //  https://leetcode.com/problems/course-schedule/
 
 function canFinish(numCourses, prerequisites) {
+  const edges = new Array(numCourses).fill(0);
+  const coursesWithNoPrereqs = [];
 
+  for (let [course] of prerequisites) {
+    edges[course]++;
+  }
+
+  for (let i = 0; i < edges.length; i++) {
+    if (edges[i] === 0) {
+      coursesWithNoPrereqs.push(i);
+    }
+  }
+
+  let coursesTaken = 0;
+
+  while (coursesWithNoPrereqs.length) {
+    const course = coursesWithNoPrereqs.shift();
+    coursesTaken++;
+
+    for (const [c, p] of prerequisites) {
+      if (p === course) {
+        edges[c]--;
+        if (edges[c] === 0) {
+          coursesWithNoPrereqs.push(c);
+        }
+      }
+    }
+  }
+
+  return coursesTaken === numCourses;
 }
--- projects/graphs/graph_project/lib/num_regions.js
+++ projects/graphs/graph_project/lib/num_regions.js
@@ -1,17 +1,19 @@
 function numRegions(graph) {
-  let num = 0;
+  let regions = 0;
   let visited = new Set();
+
   for (let node in graph) {
-    if (isNewRegion(node, graph, visited)) num++;
+    if (checkRegion(node, graph, visited)) regions++;
   }
-  return num;
+
+  return regions;
 }
 
-function isNewRegion(node, graph, visited) {
+function checkRegion(node, graph, visited) {
   if (visited.has(node)) return false;
   visited.add(node);
 
-  graph[node].forEach((neighbor) => isNewRegion(neighbor, graph, visited));
+  graph[node].forEach((neighbor) => checkRegion(neighbor, graph, visited));
 
   return true;
 }
