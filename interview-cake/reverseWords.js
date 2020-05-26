8b1ada8acab179e78ffaefdff7a6f3bc8c1500fa
Annie <annieg4123@gmail.com>
2020-05-26 16:44:55 -0700

    Add comments to reverseWord

--- interview-cake/reverseWords.js
+++ interview-cake/reverseWords.js
@@ -1,4 +1,8 @@
-function reverseWords(message) {
+//www.interviewcake.com/question/javascript/reverse-words
+
+// time = O(n), space = O(1)
+
+https: function reverseWords(message) {
   reverse(message, 0, message.length - 1);
 
   let start = 0;
@@ -28,46 +32,46 @@ function reverse(arr, start, end) {
 
 // Tests
 
-let desc = 'one word';
-let input = 'vault'.split('');
+let desc = "one word";
+let input = "vault".split("");
 reverseWords(input);
-let actual = input.join('');
-let expected = 'vault';
+let actual = input.join("");
+let expected = "vault";
 assertEqual(actual, expected, desc);
 
-desc = 'two words';
-input = 'thief cake'.split('');
+desc = "two words";
+input = "thief cake".split("");
 reverseWords(input);
-actual = input.join('');
-expected = 'cake thief';
+actual = input.join("");
+expected = "cake thief";
 assertEqual(actual, expected, desc);
 
-desc = 'three words';
-input = 'one another get'.split('');
+desc = "three words";
+input = "one another get".split("");
 reverseWords(input);
-actual = input.join('');
-expected = 'get another one';
+actual = input.join("");
+expected = "get another one";
 assertEqual(actual, expected, desc);
 
-desc = 'multiple words same length';
-input = 'rat the ate cat the'.split('');
+desc = "multiple words same length";
+input = "rat the ate cat the".split("");
 reverseWords(input);
-actual = input.join('');
-expected = 'the cat ate the rat';
+actual = input.join("");
+expected = "the cat ate the rat";
 assertEqual(actual, expected, desc);
 
-desc = 'multiple words different lengths';
-input = 'yummy is cake bundt chocolate'.split('');
+desc = "multiple words different lengths";
+input = "yummy is cake bundt chocolate".split("");
 reverseWords(input);
-actual = input.join('');
-expected = 'chocolate bundt cake is yummy';
+actual = input.join("");
+expected = "chocolate bundt cake is yummy";
 assertEqual(actual, expected, desc);
 
-desc = 'empty string';
-input = ''.split('');
+desc = "empty string";
+input = "".split("");
 reverseWords(input);
-actual = input.join('');
-expected = '';
+actual = input.join("");
+expected = "";
 assertEqual(actual, expected, desc);
 
 function assertEqual(a, b, desc) {
