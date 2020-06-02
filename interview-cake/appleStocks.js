279e8a26a9774e37ccaa604da75f446d3ebec86d
Annie <annieg4123@gmail.com>
2020-06-02 13:04:57 -0700

    Finish appleStocks

--- interview-cake/appleStocks.js
+++ interview-cake/appleStocks.js
@@ -1,7 +1,19 @@
 function getMaxProfit(stockPrices) {
   // Calculate the max profit
+  if (stockPrices.length < 2)
+    throw new Error("Can't make a profit with less than 2 prices");
 
-  return 0;
+  let minPrice = stockPrices[0];
+  let maxProfit = stockPrices[1] - stockPrices[0];
+
+  for (let i = 1; i < stockPrices.length; i++) {
+    const currentPrice = stockPrices[i];
+    const potentialProfit = currentPrice - minPrice;
+    maxProfit = Math.max(maxProfit, potentialProfit);
+    minPrice = Math.min(minPrice, currentPrice);
+  }
+
+  return maxProfit;
 }
 
 // Tests
